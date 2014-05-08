8f7daa10882cec3e7513c162512839354d59d4da
Matthew Beyer <matthewbeyer@fullsail.edu>
2014-05-08 08:31:43 -0600

    changes

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,17 +1,17 @@
 $(function(){
 
-	$('a').click(function(e){
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-		        $('html,body').animate({
-		          scrollTop: target.offset().top
-		        }, 500);
-	      	}
-	    }
-	    e.preventDefault();
-	});
+	$('a').click(function(e) {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 500);
+      e.preventDefault();
+    }
+  }
+});
 
 	$('nav#main').affix({
       offset: {
